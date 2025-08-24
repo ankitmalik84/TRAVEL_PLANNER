e8a831640bcda223c6f0a18c12de12195244b1ea
ankitmalik84 <ankitmalik844903@gmail.com>
2025-08-24 23:05:34 +0530

    refactor: Enhance travel planning functionality by implementing streaming message handling in main.py and adding max_turns

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 from autogen_agentchat.messages import TextMessage
+from autogen_agentchat.base import TaskResult
 from teams.travel_team import team
 import asyncio
 
 async def main():
     task = TextMessage(content="Plan a trip to Paris for 5 days.",source="User")
-    result = await team.run(task=task)
-
-    for message in result.messages:
-        print(f"{message.source}: {message.content}")
+    async for message in team.run_stream(task=task):
+        if isinstance(message,TaskResult):
+                message = f'Interview completed with result: {message.stop_reason}'
+                print(message)
+        else:
+                message = f'{message.source}: {message.content}'
+                print(message)
 
 
 if __name__ == "__main__":
--- teams/travel_team.py
+++ teams/travel_team.py
@@ -4,12 +4,13 @@ from autogen_agentchat.teams import RoundRobinGroupChat
 from agents.planner import planner_agent
 from agents.researcher import research_agent
 
-from config.settings import TERMINATION_WORD
+from config.settings import MAX_TURN
 from utils.utils import get_termination_condition
 
 team = RoundRobinGroupChat(
     participants=[planner_agent, research_agent],
     name="Travel Planning Team",
     description="A collaborative team that helps users plan their trips by researching destinations, creating itineraries, and providing travel tips. The team consists of a planner agent and a research agent working together in a round-robin fashion.",
-    termination_condition=get_termination_condition()
+    termination_condition=get_termination_condition(),
+    max_turns=MAX_TURN
 )
